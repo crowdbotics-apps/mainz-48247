4c5c5cff24848bdc029b9efdea52941e7fb7f770
Crowdbotics <team@crowdbotics.com>
2024-06-04 03:41:44 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mainz-48247",
+  url: "https://mainz-48247.botics.co",
+  api: "https://mainz-48247.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
